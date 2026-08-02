bcc2a27f663ed9a91696e81506611286d0725fc8
Lim Joe Quan <joequanlim@gmail.com>
2026-08-02 23:42:42 +0800

    feat(code) start implementing gameboard factory

--- src/game.js
+++ src/game.js
@@ -12,3 +12,38 @@ export function createShip(length) {
     }
   }
 }
+
+export function createGameboard() {
+  return {
+    ships: [],
+    misses: [],
+    placeShip(length, direction, x, y) {
+      if (x + length > 10 || y + length > 10) {
+        return null
+      }
+      createShip(length)
+      const coords = []
+      for (let i = 0; i < length; i++) {
+        if (direction === 'horizontal') {
+          coords.push({x: x + i, y})
+        } else {
+          coords.push({x, y: y + i})
+        }
+      }
+      this.ships.push(coords)
+      return this
+    },
+    receiveAttack(targetX, targetY) {
+      for (const ship of ships) {
+        for (const coord of ship) {
+          if (coord.x === targetX && coord.y === targetY) {
+            ship.hit()
+            return ship.hits
+          }
+        }
+      }
+      this.misses.push({targetX, targetY})
+      return 'miss at (' + targetX + ' , ' + targetY + ')'
+    }
+  }
+}
--- src/game.test.js
+++ src/game.test.js
@@ -1,4 +1,4 @@
-import { createShip } from "./game.js";
+import { createGameboard, createShip } from "./game.js";
 
 test('create ship that is 3 spots long', () => {
   expect(createShip(3).length).toBe(3)
@@ -10,4 +10,16 @@ test('increase hits after using hit()', () => {
 
 test('mark ship as sunk if hits >= length', () => {
   expect(createShip(2).hit().hit().checkSunk()).toBe(true)
+})
+
+test('prevent placing ships out of the gameboard', () => {
+  expect(createGameboard().placeShip(2, 'horizontal', 11, 12)).toBe(null)
+})
+
+test('prevent placing ships that extend out of the gameboard (but originate within the gameboard', () => {
+  expect(createGameboard().placeShip(4, 'vertical', 6, 8)).toBe(null)
+})
+
+test('create ship correctly in gameboard', () => {
+  expect(createGameboard().placeShip(2, 'horizontal', 2, 5).ships).toStrictEqual([[{"x": 2, "y": 5}, {"x": 3, "y": 5}]])
 })
