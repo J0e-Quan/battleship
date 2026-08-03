115cdc921020c65cd431e531bd89fa9848126abc
Lim Joe Quan <joequanlim@gmail.com>
2026-08-03 22:28:24 +0800

    feat(ui): render player ships

--- src/ui.js
+++ src/ui.js
@@ -7,25 +7,32 @@ renderGameboard('human')
 renderGameboard('computer')
 
 // temporary hardcoded ship placements
-human.gameboard.placeShip(1, 'horizontal', 2, 3).placeShip(2, 'vertical', 4, 1).placeShip(3, 'vertical', 6, 3).placeShip(4, 'horizontal', 1, 8).placeShip(5, 'horizontal', 3, 6)
+human.gameboard.placeShip(1, 'horizontal', 2, 3).placeShip(2, 'vertical', 4, 1).placeShip(3, 'vertical', 6, 3).placeShip(4, 'horizontal', 3, 9).placeShip(5, 'horizontal', 5, 7)
 computer.gameboard.placeShip(1, 'horizontal', 8, 9).placeShip(2, 'vertical', 2, 1).placeShip(3, 'vertical', 7, 3).placeShip(4, 'horizontal', 3, 8).placeShip(5, 'horizontal', 1, 6)
-
+renderHumanShips()
 
 function renderGameboard(type) {
   const targetId = type + '-gameboard'
   const gameboard = document.getElementById(targetId)
-  console.log(gameboard)
-  for (let i = 1; i < 11; i++) {
+  for (let y = 1; y < 11; y++) {
     const row = document.createElement('div')
     row.classList.add('row')
-    row.id = 'y-' + i
-    for (let i = 1; i < 11; i++) {
+    for (let x = 1; x < 11; x++) {
       const point = document.createElement('button')
       point.type = 'button'
       point.classList.add('point')
-      point.id = 'x-' + i
+      point.id = type + '-' + x + '-' + y
       row.appendChild(point)
     }
     gameboard.appendChild(row)
+  }
+}
+
+function renderHumanShips() {
+  for (const ship of human.gameboard.ships) {
+    for (const coord of ship.coordinates) {
+      const point = document.getElementById('human-' + coord.x + '-' + coord.y)
+      point.classList.add('ship')
+    }
   }
 }
